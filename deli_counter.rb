3915f489987d347382730486a26c1d716568d5f8
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-07 17:34:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 # Write your code here.
-katz_deli = []
+deli = []
 def line(deli)
   if deli.empty?
     puts "The line is currently empty."
@@ -10,4 +10,9 @@ def line(deli)
     end 
     puts current_line
   end 
+end 
+
+def take_a_number(deli, name)
+  deli << name 
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
 end 
